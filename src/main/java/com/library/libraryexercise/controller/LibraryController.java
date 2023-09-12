74759665da46e8601fb1a4eb9b6eda21d95910bd
Kacper Hanżek <kacperhanzek@gmail.com>
2023-09-12 11:43:06 +0200

    Poprawienie metody get oraz dodanie testów

--- src/main/java/com/library/libraryexercise/controller/LibraryController.java
+++ src/main/java/com/library/libraryexercise/controller/LibraryController.java
@@ -3,32 +3,34 @@ package com.library.libraryexercise.controller;
 
 import com.library.libraryexercise.controller.dto.Book;
 import com.library.libraryexercise.controller.exceptions.BookNotFoundException;
-import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
 @RestController
 @RequestMapping("/library")
 public class LibraryController {
 
-    private final Map<String, Book> books = new HashMap<>();
+    private final Map<String, Book> books = new HashMap<>() {{
+        put("Harry Potter", new Book("Harry Potter i więzień Azkabanu", "J.K. Rowling", "Fantasy", 300));
+    }};;
 
-    @GetMapping("/books")
-    public Book getBook(@ParameterObject Book Book) {
-        return new Book("Harry Potter i więźien Azkabanu", "J.K.Rowling", "Fantasy", 300);
+    @GetMapping("/books/{title}")
+    public Book getBook(@PathVariable String title, @RequestParam String author) {
+        if (!books.containsKey(title)) {
+            throw new BookNotFoundException("Książka o tym tytule: " + title + " nie znajduje się w bazie");
+        }
+        return books.get(title);
     }
 
     @PutMapping("/books/{title}")
     public Book addBook(@PathVariable String title,
                         @RequestParam String author,
                         @RequestParam String genre,
-                        @RequestParam int pageCount,
-                        @ParameterObject Book book) {
+                        @RequestParam int pageCount) {
         if (books.containsKey(title)) {
             throw new IllegalArgumentException("Książka o tytule" + title + " jest już w bazie");
         }
@@ -43,12 +45,9 @@ public class LibraryController {
     }
 
     @DeleteMapping("books/{title}")
-    public String delBook(@PathVariable String title, @RequestParam String author, @ParameterObject Book book) {
-        if (!books.containsKey(title)) {
-            throw new BookNotFoundException("Książka o tym tytule: " + title + " nie znajduje się w bazie");
-        }
+    @ResponseStatus(CREATED)
+    public String delBook(@PathVariable String title, @RequestParam String author) {
         books.remove(title);
         return "Książka o tym tytule: " + title + " została usunięta";
-
     }
 }
